45c32482acd7c6fcd868eb263b43fe41004ca122
Andrés Gómez <andresgomezfrr@gmail.com>
2017-09-14 12:38:02 +0200

    WIZZIE-167 Adapt MQTT kafka connect to support String output (#2)

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ source ./VERSIONS
 
 #### MQTT Instalation ####
 rm -f -r kafka-connect-mqtt
-git clone https://github.com/evokly/kafka-connect-mqtt.git
+git clone https://github.com/wizzie-io/kafka-connect-mqtt.git
 (cd kafka-connect-mqtt; ./gradlew clean jar copyRuntimeLibs)
 cp kafka-connect-mqtt/build/libs/kafka-connect-mqtt-*.jar ./kafka-connect/libs/
 cp kafka-connect-mqtt/build/output/lib/org.eclipse.paho.client.mqttv3-*.jar ./kafka-connect/libs/
@@ -16,7 +16,7 @@ rm -f -r kafka-connect-mqtt
 #### Syslog Instalation ####
 rm -f -r kafka-connect-syslog
 git clone https://github.com/jcustenborder/kafka-connect-syslog.git
-(cd kafka-connect-syslog; git checkout 0.2.8; mvn clean package; tar -xvf target/kafka-connect-syslog-0.2-SNAPSHOT.tar.gz)
+(cd kafka-connect-syslog; git checkout 0.2.15; mvn clean package; tar -xvf target/kafka-connect-syslog-0.2-SNAPSHOT.tar.gz)
 cp -r kafka-connect-syslog/usr/share/java/kafka-connect-syslog/ ./kafka-connect/libs/
 rm -f -r kafka-connect-syslog
 
